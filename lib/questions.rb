73ba5d4ae6eff9506e11887ac71d012fb371108a
Stuart Priestley <Stuart.Priestley@bglgroup.co.uk>
2019-08-15 13:22:32 +0100

    Pass test twenty-four

--- lib/questions.rb
+++ lib/questions.rb
@@ -149,13 +149,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+	array.take_while { |comp| comp <= 5 }
 end
 
 # get all the letters used in an array of words and return
 # it as a array of letters, in alphabetical order
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
-def get_all_letters_in_array_of_words(array)
+def get_all_letters_in_array_of_words(array)	
 end
 
 # BONUS SECTION
